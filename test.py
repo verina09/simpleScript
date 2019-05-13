1f6c5d30fe007535461f0b367c4a22099779b636
Verina Cristie <cristie_verina@sutd.edu.sg>
2019-05-13 22:56:42 +0800

    add function added

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
+def add(a,b):
+    result = float(a) + float(b)
+    print("add: ",result)
+
 # Store input numbers
 num1 = input('Enter first number: ')
 num2 = input('Enter second number: ')
 
 print("num1: ", num1, " num2: ", num2)
-
+add(num1, num2)
