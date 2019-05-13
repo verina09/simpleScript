464ceefae0c244cbc5caf8b08d505ad2e6cf7e56
Verina Cristie <cristie_verina@sutd.edu.sg>
2019-05-13 22:54:21 +0800

    subtraction function added

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
+def subtract(a,b):
+    result = float(a) - float(b)
+    print("substract: ", result)
+
 # Store input numbers
 num1 = input('Enter first number: ')
 num2 = input('Enter second number: ')
 
 print("num1: ", num1, " num2: ", num2)
-
+subtract(num1,num2)
